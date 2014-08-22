7d24aa5121bf22ef4fdf8de3e940006858a1f8f8
JP Verkamp <me@jverkamp.com>
2014-08-21 17:40:23 -0700

    Add a function to render move froms

--- render.rkt
+++ render.rkt
@@ -5,9 +5,11 @@
 
 (require pict
          racket/format
+         racket/function
          racket/match
          "board.rkt"
-         "pieces.rkt")
+         "pieces.rkt"
+         "pt.rkt")
 
 ; The hash associating piece names with glyphs 
 (define current-glyphs (make-parameter #f))
@@ -27,32 +29,57 @@
      (render-glyph/pict (make-glyph g) tile-size)]))
 
 ; Render a board to a pict with the given tile size
-(define (render/pict b [tile-size 20])
-  ; Sanity check and unpack, we don't care about the actual pieces at this point
-  (when (not (current-glyphs)) (error 'render/pict "must specify (current-glyphs) as a hash of name -> glyph"))
-  (match-define (board _ board-data) b)
- 
-  ; Render each tile into a list of lists
-  (define tiles
-    (for/list ([row-index (in-naturals)]
-               [row       (in-vector board-data)])
-      (for/list ([col-index (in-naturals)]
-                 [col       (in-vector row)])
-        ; Get the background tile
-        ; TODO: Paramaterize the tile colors
-        ; TODO: Figure out how to do borders
-        (define tile (colorize (filled-rectangle 20 20) (if (even? (+ row-index col-index)) "LightGray" "DarkGray")))
-        (cond
-          ; If there is a piece here, render the piece, colorize based on the player, and overlay on the tile
-          [col
-           (match-define (list player piece) col)
-           (define glyph (render-glyph/pict (hash-ref (current-glyphs) piece) tile-size))
-           (cc-superimpose tile (colorize glyph (~a player)))]
-          ; If not, just return the empty tile
-          [else
-           tile]))))
-  
-  ; Smash together all the lists of lists
-  (apply vc-append
-         (for/list ([row (in-list tiles)])
-           (apply hc-append row))))
+; Movelists is a hash of point to color to allow for special colorings
+; If origin is specified, generate a movelist directly from that point
+(define (render/pict b #:tile-size [tile-size 20] #:highlights [special-tiles (hash)] #:move-from [origin #f])
+  (cond
+    ; Get moves from an origin
+    [origin
+     (define move-hash
+       (hash-set
+        (for/hash ([move (in-list (moves-from b origin))])
+          (values move "green"))
+        origin
+        "blue"))
+     (displayln move-hash)
+     (render/pict b #:tile-size tile-size #:highlights move-hash)]
+    ; Main rendering function
+    [else
+     ; Sanity check and unpack, we don't care about the actual pieces at this point
+     (when (not (current-glyphs)) (error 'render/pict "must specify (current-glyphs) as a hash of name -> glyph"))
+     (match-define (board _ board-data) b)
+     
+     ; Render each tile into a list of lists
+     (define tiles
+       (for/list ([row-index (in-naturals)]
+                  [row       (in-vector board-data)])
+         (for/list ([col-index (in-naturals)]
+                    [col       (in-vector row)])
+           ; Get the background tile
+           ; TODO: Paramaterize the tile colors
+           ; TODO: Figure out how to do borders
+           (define tile-color
+             (cond
+               [(hash-ref special-tiles (pt col-index row-index) #f)
+                => identity]
+               [(even? (+ row-index col-index))
+                "LightGray"]
+               [else
+                "DarkGray"]))
+           (define tile (colorize (filled-rectangle 20 20) tile-color))
+           
+           (cond
+             ; If there is a piece here, render the piece, colorize based on the player, and overlay on the tile
+             [col
+              (match-define (list player piece) col)
+              (define glyph (render-glyph/pict (hash-ref (current-glyphs) piece) tile-size))
+              (cc-superimpose tile (colorize glyph (~a player)))]
+             ; If not, just return the empty tile
+             [else
+              tile]))))
+     
+     ; Smash together all the lists of lists
+     (apply vc-append
+            (for/list ([row (in-list tiles)])
+              (apply hc-append row)))]))
+
