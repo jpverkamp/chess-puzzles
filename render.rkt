d98c7d5c7fe0d9c1f609f53c5c31fed3f69d82af
JP Verkamp <me@jverkamp.com>
2014-09-03 17:18:19 -0700

    Add player color mappings to rendering, removed move list to simplify dependencies

--- render.rkt
+++ render.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
 (provide current-glyphs
-         render/pict)
+         render)
 
 (require pict
          racket/format
@@ -29,57 +29,61 @@
      (render-glyph/pict (make-glyph g) tile-size)]))
 
 ; Render a board to a pict with the given tile size
-; Movelists is a hash of point to color to allow for special colorings
-; If origin is specified, generate a movelist directly from that point
-(define (render/pict b #:tile-size [tile-size 20] #:highlights [special-tiles (hash)] #:move-from [origin #f])
-  (cond
-    ; Get moves from an origin
-    [origin
-     (define move-hash
-       (hash-set
-        (for/hash ([move (in-list (moves-from b origin))])
-          (values move "green"))
-        origin
-        "blue"))
-     (displayln move-hash)
-     (render/pict b #:tile-size tile-size #:highlights move-hash)]
-    ; Main rendering function
-    [else
-     ; Sanity check and unpack, we don't care about the actual pieces at this point
-     (when (not (current-glyphs)) (error 'render/pict "must specify (current-glyphs) as a hash of name -> glyph"))
-     (match-define (board _ board-data) b)
-     
-     ; Render each tile into a list of lists
-     (define tiles
-       (for/list ([row-index (in-naturals)]
-                  [row       (in-vector board-data)])
-         (for/list ([col-index (in-naturals)]
-                    [col       (in-vector row)])
-           ; Get the background tile
-           ; TODO: Paramaterize the tile colors
-           ; TODO: Figure out how to do borders
-           (define tile-color
+; highlights is a hash of point to color to allow for special colorings
+; player-colors is either a hash of player to color names or a function of the same
+(define (render b 
+                #:tile-size [tile-size 20] 
+                #:highlights [special-tiles (hash)] 
+                #:mode [mode 'bitmap]
+                #:player-colors [player-colors (hash)])
+  ; Sanity check and unpack, we don't care about the actual pieces at this point
+  (when (not (current-glyphs)) (error 'render "must specify (current-glyphs) as a hash of name -> glyph"))
+  (match-define (board _ board-data) b)
+  
+  ; Render each tile into a list of lists
+  (define tiles
+    (for/list ([row-index (in-naturals)]
+               [row       (in-vector board-data)])
+      (for/list ([col-index (in-naturals)]
+                 [col       (in-vector row)])
+        ; Get the background tile
+        ; TODO: Paramaterize the tile colors
+        ; TODO: Figure out how to do borders
+        (define tile-color
+          (cond
+            [(hash-ref special-tiles (pt col-index row-index) #f)
+             => identity]
+            [(even? (+ row-index col-index))
+             "LightGray"]
+            [else
+             "DarkGray"]))
+        (define tile (colorize (filled-rectangle 20 20) tile-color))
+        
+        (cond
+          ; If there is a piece here, render the piece, colorize based on the player, and overlay on the tile
+          [col
+           (match-define (list player piece) col)
+           (define glyph (render-glyph/pict (hash-ref (current-glyphs) piece) tile-size))
+           (define player-color 
              (cond
-               [(hash-ref special-tiles (pt col-index row-index) #f)
-                => identity]
-               [(even? (+ row-index col-index))
-                "LightGray"]
+               [(hash? player-colors)
+                (hash-ref player-colors player (~a player))]
+               [(procedure? player-colors)
+                (player-colors player)]
                [else
-                "DarkGray"]))
-           (define tile (colorize (filled-rectangle 20 20) tile-color))
+                (~a player)]))
            
-           (cond
-             ; If there is a piece here, render the piece, colorize based on the player, and overlay on the tile
-             [col
-              (match-define (list player piece) col)
-              (define glyph (render-glyph/pict (hash-ref (current-glyphs) piece) tile-size))
-              (cc-superimpose tile (colorize glyph (~a player)))]
-             ; If not, just return the empty tile
-             [else
-              tile]))))
-     
-     ; Smash together all the lists of lists
-     (apply vc-append
-            (for/list ([row (in-list tiles)])
-              (apply hc-append row)))]))
+           (cc-superimpose tile (colorize glyph player-color))]
+          ; If not, just return the empty tile
+          [else
+           tile]))))
+  
+  ; Smash together all the lists of lists
+  ; Make it a bitmap for performance reasons
+  ((case mode
+     [(bitmap) pict->bitmap]
+     [(pict)   identity])
+   (apply vc-append
+          (for/list ([row (in-list tiles)])
+            (apply hc-append row)))))
 
